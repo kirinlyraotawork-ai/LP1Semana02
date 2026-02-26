da59eb786c5530fb8a0c856a8ee6effcdaec9f40
Kirin <kirinlyraota.work@gmail.com>
2026-02-26 16:17:39 +0000

    questions added on TempConvert

---
+++ TempConvert/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Globalization;
+namespace TempConvert
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string question, response; 
+            CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+            Console.Write("Temperature:");
+            string temp_1 = Console.ReadLine();
+            Console.Write("Unit(C/F):");
+            question = Console.ReadLine();
+            switch (question)
+            {
+                case "C":
+                    response = "...C = ...F";
+                    break;
+                case "F":
+                    response = " ...F = ...C";
+                    break;
+                default:
+                    response = "I didn't understand you. Can you please repeat";
+                    break;
+            
+        }
+        Console.WriteLine(response);
+    }
+    }
+}
+
