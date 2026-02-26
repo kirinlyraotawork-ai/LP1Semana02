73b6a995a8fee8ea8cb4161f099b2d408b09eec3
Kirin <kirinlyraota.work@gmail.com>
2026-02-26 00:08:55 +0000

    last alterations for exercise 6

---
+++ ValoresEspeciais/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ValoresEspeciais
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine($"{byte.MinValue}");
+            Console.WriteLine($"{byte.MaxValue}");
+            Console.WriteLine($"{sbyte.MinValue}");
+            Console.WriteLine($"{sbyte.MaxValue}");
+            Console.WriteLine($"{short.MinValue}");
+            Console.WriteLine($"{short.MaxValue}");
+            Console.WriteLine($"{int.MinValue}");
+            Console.WriteLine($"{int.MaxValue}");
+            Console.WriteLine($"{long.MinValue}");
+            Console.WriteLine($"{long.MaxValue}");
+            Console.WriteLine($"{float.MinValue}");
+            Console.WriteLine($"{float.MaxValue}");
+            Console.WriteLine($"{double.MinValue}");
+            Console.WriteLine($"{double.MaxValue}");
+            Console.WriteLine($"{decimal.MinValue}");
+            Console.WriteLine($"{decimal.MaxValue}");
+            Console.WriteLine($"{char.MinValue}");
+            Console.WriteLine($"{char.MaxValue}");
+            Console.WriteLine($"{ulong.MinValue}");
+            Console.WriteLine($"{ulong.MaxValue}");
+            Console.WriteLine($"{float.PositiveInfinity}");
+            Console.WriteLine($"{float.NegativeInfinity}");
+            Console.WriteLine($"{float.NaN}");
+            Console.WriteLine($"{double.PositiveInfinity}");
+            Console.WriteLine($"{double.NegativeInfinity}");
+            Console.WriteLine($"{double.NaN}");
+        }
+    }
+}
