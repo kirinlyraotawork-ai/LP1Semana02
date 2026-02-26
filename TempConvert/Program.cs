d5a0c1a157259e163bc9caa4cf6dd58ba85b49f0
Kirin <kirinlyraota.work@gmail.com>
2026-02-26 16:48:07 +0000

    added absolute value on TempConvert

--- TempConvert/Program.cs
+++ TempConvert/Program.cs
@@ -7,7 +7,7 @@ namespace TempConvert
     {
         private static void Main(string[] args)
         {
-            string question_1, response, question_2; 
+            string question_1, response, response_2; 
             CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
 
             Console.Write("Temperature: ");
@@ -24,16 +24,23 @@ namespace TempConvert
             {
                 case "C":
                     response = $"{conversion:f2} C = {a_1:f2} F";
+                    response_2 = $"Absolute value: {Math.Abs(conversion * 1.8 + 32):f2}";
+                    Console.WriteLine(response);
+                    Console.WriteLine(response_2);
                     break;
                 case "F":
                     response = $"{conversion:f2} F = {a_2:f2} C";
+                    response_2 = $"Absolute value: {Math.Abs((conversion-32)/1.8):f2}";
+                    Console.WriteLine(response);
+                    Console.WriteLine(response_2);
                     break;
                 default:
                     response = "Invalid unit.";
+                    Console.WriteLine(response);
                     break;
-            
+        
         }
-        Console.WriteLine(response);
+    
 
         
     }
